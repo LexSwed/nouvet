b893c072d3f88d784a9265d1af458b860d1810d3
LexSwed <lexswed@gmail.com>
2023-11-18 16:03:36 +0100

    simple homepage

--- app/routes/_www+/index.tsx
+++ app/routes/_www+/index.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import i18n from "~/i18n/i18n.server.ts";
 import { Link } from "@remix-run/react";
 import { Button } from "~/lib/ui/button.tsx";
+import { ArrowCircleUpRight } from "~/lib/icons/arrow-circle-up-right.tsx";
 
 export const meta: MetaFunction = () => {
 	return [
@@ -26,31 +27,22 @@ export default function IndexRoute() {
 					<Link to="/family">{t("open-family-app")}</Link>
 				</Button>
 			</header>
-			<ul>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/blog"
-						rel="noreferrer"
+			<main className="container mt-12 flex flex-col gap-8">
+				<section className="flex flex-col gap-8">
+					<h1 className="text-3xl font-bold">{t("headline")}</h1>
+					<h2 className="text-xl">{t("subheadline")}</h2>
+					<Button
+						asChild
+						size="lg"
+						className="flex items-center gap-4 self-start text-lg"
 					>
-						{t("headline")}
-					</a>
-				</li>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/jokes"
-						rel="noreferrer"
-					>
-						Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-						Remix Docs
-					</a>
-				</li>
-			</ul>
+						<Link to="/family">
+							{t("cta-start")}{" "}
+							<ArrowCircleUpRight className="h-6 w-6 shrink-0" />
+						</Link>
+					</Button>
+				</section>
+			</main>
 		</div>
 	);
 }
