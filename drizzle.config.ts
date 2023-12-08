e3aa443890563ef692d6e2e174de366cfa71eef0
LexSwed <lexswed@gmail.com>
2023-12-08 22:37:13 +0100

    add db config

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+
+import { type Config } from 'drizzle-kit';
+
+export default {
+  schema: './src/db/schema.ts',
+  out: './drizzle/migrations',
+  driver: 'better-sqlite',
+  dbCredentials: {
+    url: String(process.env.DB),
+  },
+} satisfies Config;
