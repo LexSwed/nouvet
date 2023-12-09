b99273d02fffc759f23b464d8d983a06f0ddcc65
LexSwed <lexswed@gmail.com>
2023-12-09 11:30:19 +0100

    remove solid-devtools

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,3 @@
-import 'solid-devtools/setup';
 import { mount, StartClient } from '@solidjs/start/client';
 
 mount(() => <StartClient />, document.getElementById('app'));
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 // @ts-expect-error not typed
 import { defineConfig } from '@solidjs/start/config';
-import devtools from 'solid-devtools/vite';
 import { type UserConfig } from 'vite';
 import createSvgSpritePlugin from 'vite-plugin-svg-sprite';
 
 const config = {
   plugins: [
-    devtools({
-      /* features options - all disabled by default */
-      autoname: true, // e.g. enable autoname
-    }),
     createSvgSpritePlugin({
       include: '**/src/assets/icons/*.svg',
       symbolId: 'icon-[name]-[hash]',
