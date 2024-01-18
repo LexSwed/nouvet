4606ca49c36cb963aa5d66d84991ac484b5a33e3
LexSwed <lexswed@gmail.com>
2024-01-18 20:07:42 +0100

    start using migrations

--- packages/web/drizzle.config.ts
+++ packages/web/drizzle.config.ts
@@ -4,7 +4,7 @@ import { type Config } from 'drizzle-kit';
 
 export default {
   schema: './src/server/db/schema.ts',
-  out: './drizzle/migrations',
+  out: './.drizzle/migrations',
   driver: 'better-sqlite',
   dbCredentials: {
     url: String(process.env.DB),
