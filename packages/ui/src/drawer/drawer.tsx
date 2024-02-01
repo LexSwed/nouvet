926d94487c29415c50d5d0c905d59771569f6839
LexSwed <lexswed@gmail.com>
2024-02-01 19:49:59 +0100

    update drawer implementation

--- packages/ui/src/drawer/drawer.tsx
+++ packages/ui/src/drawer/drawer.tsx
@@ -4,6 +4,7 @@ import { createMediaQuery } from '@solid-primitives/media';
 import { mergeRefs } from '@solid-primitives/refs';
 import {
   children,
+  createSignal,
   Match,
   Show,
   splitProps,
@@ -20,50 +21,64 @@ import { composeEventHandlers } from '../utils';
 import cssStyles from './drawer.module.css';
 
 const Content = (ownProps: ComponentProps<'div'>) => {
+  const [open, setOpen] = createSignal(false);
   const [local, props] = splitProps(ownProps, ['children', 'class']);
-  const context = Corvu.useDialogContext();
+  let popoverEl: HTMLElement | null;
 
   return (
-    <Corvu.Content
-      as="div"
-      forceMount
-      popover="auto"
+    <Corvu.Root
+      open={open()}
+      closeOnEscapeKeyDown={false}
+      closeOnOutsidePointer={false}
+      trapFocus={false}
+      restoreFocus={false}
       role="dialog"
-      tabIndex={0}
-      class={tw(cssStyles.drawer, local.class)}
-      onBeforeToggle={composeEventHandlers(props.onBeforeToggle, (event) => {
-        context.setOpen(event.newState === 'open');
-      })}
-      onToggle={composeEventHandlers(props.onToggle, (event) => {
-        if (event.newState === 'open') {
-          (event.currentTarget as HTMLElement).focus();
-        }
-      })}
-      onFocusOut={composeEventHandlers(props.onFocusOut, (event) => {
-        if (
-          !(event.currentTarget as HTMLElement).contains(
-            event.relatedTarget as Node,
-          )
-        ) {
-          (event.currentTarget as HTMLElement).hidePopover();
-        }
-      })}
-      {...(props as ComponentProps<typeof Corvu.Content>)}
+      onOpenChange={(open) => {
+        // swiped away
+        setOpen(open);
+        if (!popoverEl?.matches(':popover-open')) popoverEl?.hidePopover();
+      }}
     >
-      <Show when={context.open()}>
-        <div class="grid w-full place-content-center pb-3 pt-2">
-          <div class="bg-on-surface/30 h-1 w-8 rounded-full" />
-        </div>
-        {local.children}
-      </Show>
-    </Corvu.Content>
+      <Corvu.Content
+        as="div"
+        forceMount
+        popover="auto"
+        role="dialog"
+        tabIndex={0}
+        class={tw(cssStyles.drawer, local.class)}
+        onToggle={composeEventHandlers(props.onToggle, (event) => {
+          setOpen(event.newState === 'open');
+          if (event.newState === 'open') {
+            (event.currentTarget as HTMLElement).focus();
+          }
+        })}
+        onFocusOut={composeEventHandlers(props.onFocusOut, (event) => {
+          if (
+            !(event.currentTarget as HTMLElement).contains(
+              event.relatedTarget as Node,
+            )
+          ) {
+            (event.currentTarget as HTMLElement).hidePopover();
+          }
+        })}
+        {...(props as ComponentProps<typeof Corvu.Content>)}
+        // @ts-expect-error ref element mismatch
+        ref={mergeRefs(ownProps.ref, (el: HTMLElement) => (popoverEl = el))}
+      >
+        <Show when={open()}>
+          <div class="grid w-full place-content-center pb-3 pt-2">
+            <div class="bg-on-surface/30 h-1 w-8 rounded-full" />
+          </div>
+          {local.children}
+        </Show>
+      </Corvu.Content>
+    </Corvu.Root>
   );
 };
 
 const Drawer = (
   ownProps: Omit<ComponentProps<typeof Popover>, 'children'> & ParentProps,
 ) => {
-  let popoverEl: HTMLElement | null;
   const isMobile = createMediaQuery(
     // @screen(sm)
     '(max-width: 640px)',
@@ -74,25 +89,7 @@ const Drawer = (
   return (
     <Switch>
       <Match when={isMobile()}>
-        <Corvu.Root
-          closeOnEscapeKeyDown={false}
-          closeOnOutsidePointer={false}
-          trapFocus={false}
-          restoreFocus={false}
-          role="dialog"
-          onOpenChange={(open) => {
-            // swiped away
-            if (!open && popoverEl?.matches(':popover-open')) {
-              popoverEl.hidePopover();
-            }
-          }}
-        >
-          <Content
-            {...props}
-            children={child()}
-            ref={mergeRefs(ownProps.ref, (el: HTMLElement) => (popoverEl = el))}
-          />
-        </Corvu.Root>
+        <Content {...props} children={child()} />
       </Match>
       <Match when={!isMobile()}>
         <Popover {...props} id={props.id}>
