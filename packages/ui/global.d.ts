b36b4b9ef73797fe429586cd4b2ce2358890aac9
LexSwed <lexswed@gmail.com>
2024-02-17 18:37:47 +0100

    corvu upgrades

--- packages/ui/global.d.ts
+++ packages/ui/global.d.ts
@@ -1,6 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module 'corvu/drawer' {
-  import DrawerType from 'corvu/dist/primitives/drawer';
-  export default DrawerType;
-}
--- packages/ui/src/drawer/drawer.tsx
+++ packages/ui/src/drawer/drawer.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { createMediaQuery } from '@solid-primitives/media';
 import { mergeRefs } from '@solid-primitives/refs';
 import {
