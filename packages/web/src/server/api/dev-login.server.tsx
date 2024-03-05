5bbb230d324a368b905a93e0c80483cf90c9bed8
LexSwed <lexswed@gmail.com>
2024-03-05 20:21:39 +0100

    fix dev login

--- packages/web/src/server/api/dev-login.server.tsx
+++ packages/web/src/server/api/dev-login.server.tsx
@@ -1,32 +1,28 @@
 'use server';
 
-import { json } from '@solidjs/router';
 import { getRequestEvent } from 'solid-js/web';
-import { deleteCookie, getCookie, sendRedirect } from 'vinxi/http';
+import { deleteCookie, getCookie } from 'vinxi/http';
 
 import { createUserSession } from '~/server/auth/user-session';
 import { RETURN_URL_COOKIE } from '~/server/const';
 
-export async function loginDevServer(formData: FormData) {
+export async function loginDevServer(name: string) {
   const returnUrl = getCookie(RETURN_URL_COOKIE);
 
   if (returnUrl) {
     deleteCookie(RETURN_URL_COOKIE);
   }
-  const name = formData.get('name');
-  if (!name) {
-    return json({ failed: true }, { status: 500, revalidate: [] });
-  }
   try {
     const event = getRequestEvent();
     await createUserSession(event!, {
-      id: name.toString(),
+      id: name,
       locale: 'en-GB',
       measurementSystem: 'metrical',
     });
 
-    return sendRedirect(returnUrl || '/app');
+    return { redirectUrl: returnUrl || '/app' };
   } catch (error) {
     console.error(error);
+    throw error;
   }
 }
--- packages/web/src/server/api/dev-login.tsx
+++ packages/web/src/server/api/dev-login.tsx
@@ -1,8 +1,20 @@
-import { action } from '@solidjs/router';
+import { action, json, redirect } from '@solidjs/router';
 
 import { loginDevServer } from './dev-login.server';
 
 export const loginDev = action(async (formData: FormData) => {
   'use server';
-  return loginDevServer(formData);
+  const name = formData.get('name')?.toString().trim();
+  if (!name) {
+    return json(
+      { errors: { name: 'Cannot be empty ' } },
+      { status: 500, revalidate: [] },
+    );
+  }
+  try {
+    const { redirectUrl } = await loginDevServer(name);
+    return redirect(redirectUrl);
+  } catch (error) {
+    return json({ failed: true }, { status: 500, revalidate: [] });
+  }
 }, 'dev-login');
