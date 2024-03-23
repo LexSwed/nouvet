5d1d0ac760eadfc0aff8bac33e61e3372a41c6e0
LexSwed <lexswed@gmail.com>
2024-03-23 11:27:06 +0100

    add account menu translations

--- packages/web/src/lib/account-menu.tsx
+++ packages/web/src/lib/account-menu.tsx
@@ -27,7 +27,7 @@ const AccountMenu = (props: AccountMenu) => {
       <Popover
         id="account-menu"
         placement="bottom-end"
-        class="bg-surface flex max-h-96 w-[15rem] flex-col rounded-3xl p-2"
+        class="bg-surface flex max-h-96 w-60 flex-col rounded-3xl p-2"
       >
         <div class="grid grid-cols-[auto_1fr] items-center gap-x-4 gap-y-1 px-3 py-4 pt-2">
           <Avatar
@@ -42,18 +42,18 @@ const AccountMenu = (props: AccountMenu) => {
             <Icon use="user-circle" size="sm" />
             {t('account-menu.account')}
           </MenuItem>
-          <MenuItem class="gap-2">
+          <MenuItem>
             <Icon use="install" size="sm" />
-            Install for offline
+            <Text as="p">{t('account-menu.pwa-title')}</Text>
           </MenuItem>
           <MenuItem as={A} href="/help">
-            <Icon use="support" size="sm" />
-            Support
+            <Icon use="seal-warning" size="sm" />
+            {t('account-menu.support')}
           </MenuItem>
           <Divider role="separator" class="my-2" />
           <MenuItem as="a" href="/app/logout">
             <Icon use="sign-out" size="sm" />
-            Log Out
+            {t('account-menu.logout')}
           </MenuItem>
         </MenuList>
       </Popover>
