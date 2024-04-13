e3711656d462120d292f7a3ac6d57f141e977b57
LexSwed <lexswed@gmail.com>
2024-04-13 16:23:26 +0200

    reuse logout function

--- packages/web/src/routes/app/(auth)/logout.ts
+++ packages/web/src/routes/app/(auth)/logout.ts
@@ -1,11 +1,8 @@
 import { sendRedirect } from 'vinxi/http';
 
-import { useLucia } from '~/server/auth/lucia';
-import { getRequestUser } from '~/server/auth/request-user';
+import { deleteUserSession } from '~/server/auth/user-session';
 
 export const GET = async () => {
-  const user = await getRequestUser();
-  const lucia = useLucia();
-  await lucia.invalidateSession(user.sessionId);
-  return sendRedirect('/');
+  await deleteUserSession();
+  return sendRedirect('/app');
 };
