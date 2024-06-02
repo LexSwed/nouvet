2f877d990af4078bdf4300eee1b03be0c70e7a08
LexSwed <lexswed@gmail.com>
2024-06-02 12:21:39 +0200

    storybook update

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -15,13 +15,15 @@ const config = {
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-mdx-gfm'),
   ],
   framework: {
     name: getAbsolutePath('storybook-solidjs-vite') as 'storybook-solidjs-vite',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
+  docs: {},
+  core: {
+    disableTelemetry: true,
   },
 } satisfies StorybookConfig;
 
