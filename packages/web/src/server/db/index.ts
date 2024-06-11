81907fcee43b446e58c60c6cfb3b64e37ab4081f
LexSwed <lexswed@gmail.com>
2024-06-11 21:36:46 +0200

    reinitialise db on every request

--- packages/web/src/server/db/index.ts
+++ packages/web/src/server/db/index.ts
@@ -1,19 +1,9 @@
 import Database from 'better-sqlite3';
-import {
-  drizzle,
-  type BetterSQLite3Database,
-} from 'drizzle-orm/better-sqlite3';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
 
 import { env } from '../env';
 
-const sqlite = new Database(env.DB);
-let _db: BetterSQLite3Database | null = null;
-
-console.log(process.env.DB_CONNECTION);
 export const useDb = () => {
-  if (!_db) {
-    _db = drizzle(sqlite, { logger: env.DEV });
-  }
-
-  return _db;
+  const sqlite = new Database(env.DB);
+  return drizzle(sqlite, { logger: env.DEV });
 };
