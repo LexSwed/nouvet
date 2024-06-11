86dc9c9ea917605d17d2880ae4af0d872f820330
LexSwed <lexswed@gmail.com>
2024-06-11 21:38:43 +0200

    revert DB re-init

--- packages/web/src/server/db/index.ts
+++ packages/web/src/server/db/index.ts
@@ -1,9 +1,18 @@
 import Database from 'better-sqlite3';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
+import {
+  drizzle,
+  type BetterSQLite3Database,
+} from 'drizzle-orm/better-sqlite3';
 
 import { env } from '../env';
 
+const sqlite = new Database(env.DB);
+let _db: BetterSQLite3Database | null = null;
+
 export const useDb = () => {
-  const sqlite = new Database(env.DB);
-  return drizzle(sqlite, { logger: env.DEV });
+  if (!_db) {
+    _db = drizzle(sqlite, { logger: env.DEV });
+  }
+
+  return _db;
 };
