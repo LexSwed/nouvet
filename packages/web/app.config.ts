d564aa984759df10ebc634dc596ee582822be155
LexSwed <lexswed@gmail.com>
2024-06-30 18:03:16 +0200

    remove https attempts

--- packages/web/app.config.ts
+++ packages/web/app.config.ts
@@ -1,11 +1,7 @@
-import fs from "node:fs";
 import { defineConfig } from "@solidjs/start/config";
-import mkcert from "mkcert";
 import { imagetools } from "vite-imagetools";
 import viteSvgSpriteWrapper from "vite-svg-sprite-wrapper";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-const { cert, key } = await checkAndGenerateCertificates();
 
 export default defineConfig({
 	ssr: true,
@@ -15,10 +11,6 @@ export default defineConfig({
 		hot: false,
 	},
 	server: {
-		https: {
-			cert,
-			key,
-		},
 		prerender: {
 			// TODO: needs per-language generation
 			// routes: ['/', '/about', '/privacy'],
@@ -44,7 +36,7 @@ export default defineConfig({
 	},
 	vite: {
 		define: {
-			__RUNNING_ON_DEV__: process.env.NODE_ENV === "test",
+			"process.env.VITE_ACCEPTANCE_TESTING": JSON.stringify(process.env.VITE_ACCEPTANCE_TESTING),
 		},
 		css: {
 			postcss: "../config/postcss.config.cjs",
@@ -69,57 +61,3 @@ export default defineConfig({
 		],
 	},
 });
-
-async function checkAndGenerateCertificates() {
-	const CERT_FOLDER_PATH = "./.certs";
-	const CERT_FILE_NAMES = {
-		rootCA: {
-			cert: "rootCA.crt",
-			key: "rootCA-key.pem",
-		},
-		cert: {
-			cert: "cert.crt",
-			key: "key.pem",
-		},
-	};
-
-	const files = {
-		cert: `${CERT_FOLDER_PATH}/${CERT_FILE_NAMES.cert.cert}`,
-		key: `${CERT_FOLDER_PATH}/${CERT_FILE_NAMES.cert.key}`,
-	};
-	if (fs.existsSync(files.cert) && fs.existsSync(files.key)) {
-		console.info("[generate-certificates]: Certificates already exist, skipping generation");
-		return files;
-	}
-
-	console.info("[generate-certificates]: No certificates found, generating new ones...");
-
-	const { CA, cert } = await generateCertificates();
-
-	fs.mkdirSync(CERT_FOLDER_PATH, { recursive: true });
-	fs.writeFileSync(`${CERT_FOLDER_PATH}/${CERT_FILE_NAMES.rootCA.cert}`, CA.cert);
-	fs.writeFileSync(`${CERT_FOLDER_PATH}/${CERT_FILE_NAMES.rootCA.key}`, CA.key);
-	fs.writeFileSync(`${CERT_FOLDER_PATH}/${CERT_FILE_NAMES.cert.cert}`, cert.cert);
-	fs.writeFileSync(`${CERT_FOLDER_PATH}/${CERT_FILE_NAMES.cert.key}`, cert.key);
-
-	console.info("[generate-certificates]: Certificates generated successfully");
-	return files;
-}
-
-async function generateCertificates() {
-	const CA = await mkcert.createCA({
-		organization: "Localhost CA",
-		countryCode: "ES",
-		state: "Barcelona",
-		locality: "Barcelona",
-		validity: 365,
-	});
-
-	const cert = await mkcert.createCert({
-		domains: ["127.0.0.1", "localhost"],
-		ca: CA,
-		validity: 365,
-	});
-
-	return { CA, cert };
-}
