c454924bba20f1f39a0e922eceeac022fc1f16b5
LexSwed <lexswed@gmail.com>
2024-07-17 19:13:57 +0200

    storybook fix?

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,24 +1,25 @@
-import { dirname, join } from "node:path";
+import remarkGfm from "remark-gfm";
 import type { StorybookConfig } from "storybook-solidjs-vite";
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string) {
-	return dirname(require.resolve(join(value, "package.json")));
-}
 
 const config = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 	addons: [
-		getAbsolutePath("@storybook/addon-links"),
-		getAbsolutePath("@storybook/addon-essentials"),
-		getAbsolutePath("@storybook/addon-interactions"),
-		getAbsolutePath("@storybook/addon-mdx-gfm"),
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+		{
+			name: "@storybook/addon-docs",
+			options: {
+				mdxPluginOptions: {
+					mdxCompileOptions: {
+						remarkPlugins: [remarkGfm],
+					},
+				},
+			},
+		},
 	],
 	framework: {
-		name: getAbsolutePath("storybook-solidjs-vite") as "storybook-solidjs-vite",
+		name: "storybook-solidjs-vite",
 		options: {},
 	},
 	docs: {},
