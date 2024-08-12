288d0103e01aa6276b8e424d057fbedb4a968158
LexSwed <lexswed@gmail.com>
2024-08-12 19:12:24 +0200

    popover - drawer fixes

--- packages/ui/src/drawer/drawer.tsx
+++ packages/ui/src/drawer/drawer.tsx
@@ -13,8 +13,8 @@ import { tw } from "../tw";
 
 import { createMediaQuery } from "@solid-primitives/media";
 import { mergeRefs } from "@solid-primitives/refs";
+import css from "../popover/popover.module.css";
 import { composeEventHandlers, mergeDefaultProps } from "../utils";
-import css from "./drawer.module.css";
 
 type Side = "top" | "bottom" | "left" | "right";
 type DrawerProps<T extends ValidComponent = "div"> = PopoverProps<T> & {
