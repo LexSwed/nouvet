a92533d9c97a3783403d409a2ad80094d17c2eec
LexSwed <lexswed@gmail.com>
2024-08-23 10:37:43 +0200

    view transitions

--- packages/web/src/routes/app/pets/[petId]/edit.tsx
+++ packages/web/src/routes/app/pets/[petId]/edit.tsx
@@ -2,14 +2,15 @@ import {
 	Button,
 	ButtonLink,
 	Card,
-	Drawer,
 	Form,
 	Icon,
 	Popover,
 	Text,
 	TextField,
 	Toast,
+	startViewTransition,
 	toast,
+	tw,
 } from "@nou/ui";
 import { Title } from "@solidjs/meta";
 import {
@@ -19,7 +20,7 @@ import {
 	useAction,
 	useSubmission,
 } from "@solidjs/router";
-import { Show } from "solid-js";
+import { Show, createSignal } from "solid-js";
 
 import { AppHeader } from "~/lib/app-header";
 import { PetPicture } from "~/lib/pet-home-card";
@@ -29,6 +30,8 @@ import { getPetForEdit, updatePet } from "~/server/api/pet";
 import { getUserProfile } from "~/server/api/user";
 import type { DatabasePet } from "~/server/db/schema";
 import { T, cacheTranslations, createTranslator } from "~/server/i18n";
+
+import "./edit.module.css";
 
 export const route = {
 	preload({ params }) {
@@ -168,33 +171,76 @@ function PetUpdateForm(props: { petId: string }) {
 	);
 }
 
-function PetDeleteForm(props: { pet: { id: string; name: string } }) {
+function PetDeleteForm(props: {
+	pet: {
+		id: DatabasePet["id"];
+		name: DatabasePet["name"];
+		pictureUrl: DatabasePet["pictureUrl"];
+		gender: DatabasePet["gender"];
+		species: DatabasePet["species"];
+	};
+}) {
+	const [open, setOpen] = createSignal(false);
 	const t = createTranslator("pets");
+	const viewTransitions = {
+		get main() {
+			return { "view-transition-name": "delete-pet" };
+		},
+		get cta() {
+			return { "view-transition-name": "delete-pet-cta" };
+		},
+	};
 	return (
 		<>
-			<Button variant="ghost" tone="destructive" popoverTarget="pet-delete-form" class="mx-5">
+			<Button
+				variant="ghost"
+				tone="destructive"
+				popoverTarget="pet-delete-form"
+				class="mx-5"
+				style={open() ? undefined : viewTransitions.cta}
+			>
 				{t("edit.delete-cta")}
 			</Button>
-			<Drawer
+			<Popover
 				id="pet-delete-form"
-				heading={t("delete.heading", { petName: props.pet.name })}
-				placement="center"
+				placement="bottom-to-bottom"
+				class={tw(
+					"-mb-2 w-[anchor(width)] translate-y-0 opacity-100 transition-none backdrop:bg-on-surface/5",
+					"hidden data-[state=open]:block",
+				)}
+				onBeforeToggle={(e: ToggleEvent) => {
+					startViewTransition(() => {
+						setOpen(e.newState === "open");
+					});
+				}}
+				style={viewTransitions.main}
+				data-state={open() ? "open" : "closed"}
 			>
-				{t("delete.text", { petName: props.pet.name })}
-				<div class="mt-6 grid grid-cols-2 gap-4">
-					<Button
-						popoverTarget="pet-delete-form"
-						popoverTargetAction="hide"
-						variant="tonal"
-						tone="neutral"
-					>
-						{t("delete.cancel")}
-					</Button>
-					<Button variant="tonal" tone="destructive">
-						{t("delete.confirm")}
-					</Button>
+				<div class="flex flex-col justify-end gap-4">
+					<PetPicture pet={props.pet} class="size-10 bg-error-container text-on-error-container" />
+					<Text with="headline-3" as="header">
+						{t("delete.heading", { petName: props.pet.name })}
+					</Text>
+					<p>{t("delete.text", { petName: props.pet.name })}</p>
+					<div class="mt-6 grid grid-cols-2 gap-4">
+						<Button
+							popoverTarget="pet-delete-form"
+							popoverTargetAction="hide"
+							variant="tonal"
+							tone="neutral"
+						>
+							{t("delete.cancel")}
+						</Button>
+						<Button
+							variant="tonal"
+							tone="destructive"
+							style={open() ? viewTransitions.cta : undefined}
+						>
+							{t("delete.confirm")}
+						</Button>
+					</div>
 				</div>
-			</Drawer>
+			</Popover>
 		</>
 	);
 }
