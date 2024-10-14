a2d5ca1367fdff49994a0afad07b71765dabee84
LexSwed <lexswed@gmail.com>
2024-10-14 16:59:33 +0200

    update plugins

--- packages/config/plugins.js
+++ packages/config/plugins.js
@@ -17,7 +17,42 @@ const customPlugins = createPlugin(({ matchUtilities }) => {
 			positionAnchor: value,
 		}),
 	});
-	// matchVariant("part", (value) => `& [data-part="${value}"]`, {});
+	matchVariant("part", (value) => `& [data-part="${value}"]`, {});
+
+	const css = {
+		display: "flex",
+		"flex-direction": "row",
+		"flex-wrap": "nowrap",
+		"scrollbar-width": "none",
+		"margin-block": `calc(-1*${theme("spacing.2")})`,
+		"padding-block": `${theme("spacing.2")}`,
+		"scroll-snap-type": "x mandatory",
+		"overflow-x": "auto",
+		"&>:last-of-type": {
+			"scroll-snap-align": "end",
+		},
+		"&>*": {
+			"flex-shrink": "0",
+			"scroll-snap-align": "start",
+			"scroll-snap-stop": "always",
+		},
+	};
+	addUtilities({
+		"overflow-snap": css,
+	});
+	matchUtilities(
+		{
+			"overflow-snap": (value) => {
+				return {
+					...css,
+					"margin-inline": `calc(-1*${value})`,
+					"padding-inline": `${value}`,
+					"scroll-padding-inline": `${value}`,
+				};
+			},
+		},
+		{ values: theme("spacing") },
+	);
 });
 
 export default customPlugins;
